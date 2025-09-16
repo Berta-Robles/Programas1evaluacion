70b86aa25cb8ecb50a027ca13cda043488078fa0
Berta-Robles <bertarobcat@gmail.com>
2025-09-16 11:48:28 +0200

    Add files via upload

---
+++ Suma.py
@@ -0,0 +1,10 @@
+# -*- coding: cp1252 -*-
+def Suma():
+    #cadena=raw_input("Como te llamas? ")#lee cadena como una cadena de caracteres(texto)
+    #print("Hola "+ cadena)
+    num1=input("Dime un numero: ")#lee num1 como número
+    num2=input("Dime otro mas: ")#lee num2 como número
+    print(str(num1)+" + "+str(num2)+" = "+str(num1+num2))
+    #str() convierte un número en una cadena
+
+Suma()
